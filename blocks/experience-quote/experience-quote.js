0c5e4d9b4c54a1b3c4495aeb30244a0c1d45e3a2
Sreekanth Choudry Nalabotu <nalabotu@adobe.com>
2025-10-15 15:49:05 -0500

    changes

--- blocks/experience-quote/experience-quote.js
+++ blocks/experience-quote/experience-quote.js
@@ -1,7 +1,10 @@
 export default function decorate(block) {
+
+  console.log("sreek------", block);
   const [quoteWrapper] = block.children;
 
   const blockquote = document.createElement("blockquote");
+
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
 }
